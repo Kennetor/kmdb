75b0fe1e70daf7713d665dd946db3c9b85c5c39c
kennetor <kenneth.toer@gmail.com>
2023-04-07 04:40:10 +0200

    Desktop version hover click working as intended

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
 
-export const ListItem = ({ to, text, icon: Icon, className }) => {
+export const ListItem = ({ to, text, icon: Icon, className, onClick }) => {
+  const handleClick = () => {
+    onClick && onClick();
+  };
   return (
     <li className={`list-none text-xl ${className}`}>
-      <Link to={to}>
+      <Link to={to} onClick={handleClick}>
         <div className="min-w-[60px] h-16 mt-4">
           <Icon className="m-auto text-5xl md:block" />
         </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,3 @@
-// Icons
 import { MdHome, MdFace } from "react-icons/md";
 import { RiMovieLine, RiSlideshow4Line } from "react-icons/ri";
 import { FaSignOutAlt, FaRegMoon } from "react-icons/fa";
@@ -7,27 +6,87 @@ import { ImSearch } from "react-icons/im";
 // Components
 import { BurgerMenu } from "./Buttons";
 import { ListItem } from "./ListItem";
+import { useState } from "react";
 
 export const Nav = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleClick = () => {
+    setIsExpanded(false);
+  };
+
+  const handleMouseEnter = () => {
+    setIsExpanded(true);
+  };
   return (
     <div>
-      <BurgerMenu />
-      <div className="z-50 xl:bg-[#ff7700] hover:bg-[#ff7700] overflow-hidden h-14 xl:h-full hover:h-full w-16 fixed transition-width duration-500 xl:hover:w-[200px] hover:w-48">
+      <div
+        className={
+          isExpanded
+            ? "z-50 xl:bg-[#ff7700] hover:bg-[#ff7700] overflow-hidden h-14 xl:h-full hover:h-full w-16 fixed transition-width duration-500 xl:hover:w-[200px] hover:w-48"
+            : "overflow-hidden w-16 hover:bg-[#ff7700] xl:bg-[#ff7700] fixed h-14 hover:h-full xl:h-full hover:w-16 z-50"
+        }
+        onMouseEnter={handleMouseEnter}
+      >
+        <BurgerMenu onClick={handleClick} />
         <ul className="flex flex-col justify-between h-full gap-10 mt-20 xl:mt-8 xl:gap-20 absolute top-0 left-0 w-full">
-          <ListItem to="/home" text="Home" icon={MdHome} />
-          <ListItem to="/movie" text="Movie" icon={RiMovieLine} />
-          <ListItem to="/tv" text="Tv Show" icon={BsFillCaretRightFill} />
-
+          <ListItem
+            to="/home"
+            text="Home"
+            icon={MdHome}
+            onClick={handleClick}
+          />
+          <ListItem
+            to="/movie"
+            text="Movie"
+            icon={RiMovieLine}
+            onClick={handleClick}
+          />
+          <ListItem
+            to="/tv"
+            text="Tv Show"
+            icon={BsFillCaretRightFill}
+            onClick={handleClick}
+          />
           <div className="mt-auto flex-grow-1"></div>
-          <ListItem to="/search" text="Search" icon={ImSearch} />
-          <ListItem to="/top" text="Top 20" icon={RiSlideshow4Line} />
-          <ListItem to="/upcoming" text="Upcoming" icon={RiSlideshow4Line} />
-
+          <ListItem
+            to="/search"
+            text="Search"
+            icon={ImSearch}
+            onClick={handleClick}
+          />
+          <ListItem
+            to="/top"
+            text="Top 20"
+            icon={RiSlideshow4Line}
+            onClick={handleClick}
+          />
+          <ListItem
+            to="/upcoming"
+            text="Upcoming"
+            icon={RiSlideshow4Line}
+            onClick={handleClick}
+          />
           <div className="mt-auto flex-grow-1"></div>
-          <ListItem to="/profile" text="Profile" icon={MdFace} />
-          <ListItem to="/darkmode" text="Theme" icon={FaRegMoon} />
+          <ListItem
+            to="/profile"
+            text="Profile"
+            icon={MdFace}
+            onClick={handleClick}
+          />
+          <ListItem
+            to="/darkmode"
+            text="Theme"
+            icon={FaRegMoon}
+            onClick={handleClick}
+          />
           <div className="mb-32">
-            <ListItem to="/signout" text="Sign Out" icon={FaSignOutAlt} />
+            <ListItem
+              to="/signout"
+              text="Sign Out"
+              icon={FaSignOutAlt}
+              onClick={handleClick}
+            />
           </div>
         </ul>
       </div>
